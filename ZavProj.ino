71fdd7b29b89e281fae65f054fc4433a8b4cb55f
NikelVitek <it2116@sspu-opava.cz>
2025-01-09 18:43:07 +0100

    Úprava kolizí + aktualizace přerušení

--- ZavProj.ino
+++ ZavProj.ino
@@ -14,9 +14,9 @@ const int screenWidth = 320;
 const int screenHeight = 240;
 volatile int btn1state = 0;
 volatile int btn2state = 0;
-unsigned long lastDebounceTimeBtn1 = 0;  // For debouncing btn1
-unsigned long lastDebounceTimeBtn2 = 0;  // For debouncing btn2
-unsigned long debounceDelay = 200;       // Debounce delay (in milliseconds)
+unsigned long lastDebounceTimeBtn1 = 0;
+unsigned long lastDebounceTimeBtn2 = 0;
+unsigned long debounceDelay = 200;
 
 Adafruit_ILI9341 TFT = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
 
@@ -61,12 +61,10 @@ void menu_start() {
     if (btn1state == HIGH) {
       game1_start();
       btn1state = LOW;
-      delay(100);
     }
     if (btn2state == HIGH) {
       game2_start();
       btn2state = LOW;
-      delay(100);
     }
   }
 }
@@ -89,23 +87,22 @@ void game1_start() {
   snakeY[1] = 120;
   snakeX[2] = 140;
   snakeY[2] = 120;
-  
+
   unsigned long lastDirectionChangeTime = 500;
-  unsigned long debounceDelay = 750;
   
   while (true) {
-    btn1state = digitalRead(btn1);
-    btn2state = digitalRead(btn2);
-
     if (btn1state == HIGH && (millis() - lastDirectionChangeTime) > debounceDelay) {
       if(direction < 4){direction += 1;}
-      else{direction=1;}
+      else{direction = 1;}
       lastDirectionChangeTime = millis();
+      btn1state = LOW;
     }
+
     if (btn2state == HIGH && (millis() - lastDirectionChangeTime) > debounceDelay) {
       if(direction > 1){direction -= 1;}
       else{direction = 4;}
       lastDirectionChangeTime = millis();
+      btn2state = LOW;
     }
 
     moveSnake();
@@ -118,7 +115,7 @@ void game1_start() {
     checkFoodEaten();
 
     drawSnake();
-    delay(50);
+    delay(40);
   }
 }
 
@@ -198,9 +195,6 @@ void gameOver() {
   TFT.println("Press any button to restart or go to menu.");
   
   while (true) {
-    btn1state = digitalRead(btn1);
-    btn2state = digitalRead(btn2);
-
     if (btn1state == HIGH || btn2state == HIGH) {
       delay(1000);
       menu_start();
@@ -226,8 +220,8 @@ void drawFood() {
   TFT.fillRect(foodX, foodY, gridSize, gridSize, ILI9341_RED);
 }
 
-int ballSpeedX = 2;
-int ballSpeedY = 2;
+int ballSpeedX = (random(0,1)) ? 6 : -6;
+int ballSpeedY = (random(0,1)) ? random(-1,-5) : random(1, 5);
 int paddleWidth = 5;
 int paddleHeight = 20;
 int ballSize = 5;
@@ -241,22 +235,22 @@ void game2_start() {
   TFT.setTextColor(ILI9341_BLACK);
   TFT.setCursor(120, 120);
   TFT.println("Press Button");
+  
   int paddle1Y = 100;
   int paddle2Y = 100;
   int paddle1T, paddle2T;
 
   while (true) {
-    btn1state = digitalRead(btn1);
-    btn2state = digitalRead(btn2);
-
     if (btn2state == HIGH) {
       paddle1DirectionUp = !paddle1DirectionUp;
       delay(10); 
+      btn2state = LOW;
     }
 
     if (btn1state == HIGH) {
       paddle2DirectionUp = !paddle2DirectionUp;
       delay(10);
+      btn1state = LOW;
     }
 
     paddle1T = paddle1Y;
@@ -273,57 +267,65 @@ void game2_start() {
       paddle2Y += 5;
     }
 
-    moveBall(paddle1Y, paddle2Y);
-    checkBallCollisions(ballX, ballY, paddle1Y, paddle2Y);
-    drawPaddles(paddle1T,paddle1Y,paddle2T,paddle2Y);
-    drawBall();
-
-    delay(100);
+    moveBall(paddle1Y, paddle2Y, paddle1T, paddle1Y);
+    drawPaddles(paddle1T, paddle1Y, paddle2T, paddle2Y);
+    delay(40);
   }
 }
 
-void moveBall(int paddle2Y, int paddle1Y) {
+void moveBall(int paddle1Y, int paddle2Y, int paddle1T, int paddle2T) {
   TFT.fillRect(ballX, ballY, ballSize, ballSize, ILI9341_BLACK);
+
   ballX += ballSpeedX;
   ballY += ballSpeedY;
 
-  if (ballY <= 0 || ballY + ballSize >= screenHeight) {
+  if (ballSpeedY == 0){
+    ballSpeedY = (random(0,1)) ? random(-1,-5) : random(1, 5);
+  }
+
+  if (ballY <= 0) {
     ballSpeedY = -ballSpeedY;
+    ballY = 1;
+  }
+
+  if(ballY + ballSize >= screenHeight){
+    ballSpeedY = -ballSpeedY;
+    ballY = screenHeight - ballSize;
+
   }
 
   if (ballX <= paddleWidth && ballY >= paddle1Y && ballY <= paddle1Y + paddleHeight) {
     ballSpeedX = -ballSpeedX;
+    ballSpeedY += random(-5, 5);
+    delay(40);
   }
 
   if (ballX >= screenWidth - ballSize - paddleWidth && ballY >= paddle2Y && ballY <= paddle2Y + paddleHeight) {
     ballSpeedX = -ballSpeedX;
+    ballSpeedY += random(-5, 5);
+    delay(40);
   }
+
+  if (ballX <= 0 || ballX >= screenWidth) {
+    ballX = 160;
+    ballY = 120;
+    ballSpeedX = (random(0,1)) ? 6 : -6;
+    ballSpeedY = (random(0,1)) ? random(-1,-4) : random(1, 4);
+    paddle1Y, paddle1T, paddle2Y, paddle2T = 100;
+  }
+
+  drawBall();
 }
 
-void drawPaddles(int paddle1T,int paddle1Y,int paddle2T,int paddle2Y) {
+
+void drawPaddles(int paddle1T, int paddle1Y, int paddle2T, int paddle2Y) {
   TFT.fillRect(0, paddle1T, paddleWidth, paddleHeight, ILI9341_BLACK);
   TFT.fillRect(screenWidth - paddleWidth, paddle2T, paddleWidth, paddleHeight, ILI9341_BLACK);
+
   TFT.fillRect(0, paddle1Y, paddleWidth, paddleHeight, ILI9341_GREEN);
   TFT.fillRect(screenWidth - paddleWidth, paddle2Y, paddleWidth, paddleHeight, ILI9341_GREEN);
 }
 
 void drawBall() {
   TFT.fillRect(ballX, ballY, ballSize, ballSize, ILI9341_WHITE);
-}
-
-void checkBallCollisions(int ballX,int ballY,int paddle1Y,int paddle2Y) {
-  if (ballX <= paddleWidth && ballY >= paddle1Y && ballY <= paddle1Y + paddleHeight) {
-    ballSpeedX = -ballSpeedX;
-  }
-
-  if (ballX >= screenWidth - ballSize - paddleWidth && ballY >= paddle2Y && ballY <= paddle2Y + paddleHeight) {
-    ballSpeedX = -ballSpeedX;
-  }
-
-  if (ballY <= 0 || ballY + ballSize >= screenHeight) {
-    ballSpeedY = -ballSpeedY;
-  }
-}
-
-void game3_start() {
 }
