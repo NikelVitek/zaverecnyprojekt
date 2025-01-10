00edf4b7dc314a14587e0519e1fa135b01392f68
NikelVitek <it2116@sspu-opava.cz>
2025-01-10 21:06:04 +0100

    Added menu

--- ZavProj.ino
+++ ZavProj.ino
@@ -49,48 +49,62 @@ void setup() {
   randomSeed(analogRead(0));
   TFT.begin();
   TFT.setRotation(1);
+  menu_start();
 }
 
 void loop() {
-  menu_start();
+
 }
 
 void menu_start() {
   TFT.fillScreen(ILI9341_BLACK);
-  TFT.setCursor(120, 120);
-  TFT.println("Press Button");
+  TFT.setTextColor(ILI9341_WHITE);
+  TFT.setTextSize(2);
+
+  TFT.setCursor(50, 50);
+  TFT.println("Select a game:");
+  TFT.setCursor(50, 100);
+  TFT.println("1. Snake");
+  TFT.setCursor(50, 150);
+  TFT.println("2. Pong");
 
   while (true) {
     if (btn1state == HIGH) {
-      game1_start();
       btn1state = LOW;
+      game1_start();
+      break;
     }
     if (btn2state == HIGH) {
-      game2_start();
       btn2state = LOW;
+      game2_start();
+      break;
     }
   }
 }
 
+
 int snakeX[1000];
 int snakeY[1000];
-int snakeLength = 3;
 int direction = 1;
 int gridSize = 5;
 int foodX, foodY;
 
 void game1_start() {
   TFT.setTextColor(ILI9341_BLACK);
-  TFT.setCursor(120, 120);
-  TFT.println("Press Button");
-  drawFood();
+  TFT.setCursor(50, 50);
+  TFT.println("Select a game:");
+  TFT.setCursor(50, 100);
+  TFT.println("1. Snake");
+  TFT.setCursor(50, 150);
+  TFT.println("2. Pong");
+  int snakeLength = 3;
   snakeX[0] = 160;
   snakeY[0] = 120;
   snakeX[1] = 150;
   snakeY[1] = 120;
   snakeX[2] = 140;
   snakeY[2] = 120;
-
+  drawFood(snakeLength);
   unsigned long lastDirectionChangeTime = 500;
   
   while (true) {
@@ -108,28 +122,26 @@ void game1_start() {
       btn2state = LOW;
     }
 
-    moveSnake();
+    moveSnake(snakeLength);
 
-    if (checkCollision()) {
+    if (checkCollision(snakeLength)) {
       gameOver(1);
       return;
     }
 
-    checkFoodEaten();
-
-    drawSnake();
+    snakeLength = checkFoodEaten(snakeLength);
+    drawSnake(snakeLength);
     delay(40);
   }
 }
 
-void moveSnake() {
+void moveSnake(int snakeLength) {
   TFT.fillRect(snakeX[snakeLength - 1], snakeY[snakeLength - 1], gridSize, gridSize, ILI9341_BLACK);
   
   for (int i = snakeLength - 1; i > 0; i--) {
     snakeX[i] = snakeX[i - 1];
     snakeY[i] = snakeY[i - 1];
   }
-
   switch (direction) {
     case 1:
       snakeX[0]++;
@@ -145,34 +157,32 @@ void moveSnake() {
       break;
   }
 
-  TFT.fillRect(snakeX[0], snakeY[0], gridSize, gridSize, ILI9341_GREEN);
 
-  if (snakeLength > 3) {
-    TFT.fillRect(snakeX[snakeLength - 2], snakeY[snakeLength - 2], gridSize, gridSize, ILI9341_GREEN);
-  }
+
 }
 
-void drawSnake() {
+void drawSnake(int snakeLength) {
   for (int i = 0; i < snakeLength; i++) {
     TFT.fillRect(snakeX[i], snakeY[i], gridSize, gridSize, ILI9341_GREEN);
   }
 }
 
-void checkFoodEaten() {
+int checkFoodEaten(int snakeLength) {
   if (abs(snakeX[0] - foodX) <= gridSize && abs(snakeY[0] - foodY) <= gridSize) {
     snakeLength += 5;
     snakeScore += 10;
     TFT.fillRect(foodX, foodY, gridSize, gridSize, ILI9341_BLACK);
-    drawFood();
+    drawFood(snakeLength);
 
     for (int i = snakeLength - 1; i >= snakeLength - 5; i--) {
       snakeX[i] = snakeX[snakeLength - 6];
       snakeY[i] = snakeY[snakeLength - 6];
     }
   }
+  return snakeLength;
 }
 
-bool checkCollision() {
+bool checkCollision(int snakeLength) {
   if (snakeX[0] < 0 || snakeX[0] + gridSize > screenWidth || snakeY[0] < 0 || snakeY[0] + gridSize > screenHeight) {
     return true;
   }
@@ -227,13 +237,15 @@ void gameOver(int game) {
       snakeScore = 0;
       player1Score = 0;
       player2Score = 0;
+      btn1state = LOW;
+      btn2state = LOW;
       menu_start();
       break;
     }
   }
 }
 
-void drawFood() {
+void drawFood(int snakeLength) {
   bool validFood = false;
   while (!validFood) {
     foodX = random(0, screenWidth / gridSize) * gridSize;
@@ -258,17 +270,20 @@ int paddleHeight = 20;
 int ballSize = 5;
 int ballX = 160;
 int ballY = 120;
-
+int paddle1Y = 100;
+int paddle2Y = 100;
 bool paddle1DirectionUp = true; 
 bool paddle2DirectionUp = true; 
 
 void game2_start() {
   TFT.setTextColor(ILI9341_BLACK);
-  TFT.setCursor(120, 120);
-  TFT.println("Press Button");
+  TFT.setCursor(50, 50);
+  TFT.println("Select a game:");
+  TFT.setCursor(50, 100);
+  TFT.println("1. Snake");
+  TFT.setCursor(50, 150);
+  TFT.println("2. Pong");
 
-  int paddle1Y = 100;
-  int paddle2Y = 100;
   int paddle1T, paddle2T;
 
   while (true) {
@@ -299,14 +314,14 @@ void game2_start() {
       paddle2Y += 5;
     }
 
-    drawPaddles(paddle1T, paddle1Y, paddle2T, paddle2Y);
-    moveBall(paddle1Y, paddle2Y, paddle1T, paddle2T);
+    drawPaddles(paddle1T,paddle2T);
+    moveBall(paddle1T, paddle2T);
     Serial.println(paddle2Y);
     delay(40);
   }
 }
 
-void moveBall(int paddle1Y, int paddle2Y, int paddle1T, int paddle2T) {
+void moveBall(int paddle1T, int paddle2T) {
   TFT.fillRect(ballX, ballY, ballSize, ballSize, ILI9341_BLACK);
 
   ballX += ballSpeedX;
@@ -365,18 +380,25 @@ void moveBall(int paddle1Y, int paddle2Y, int paddle1T, int paddle2T) {
     }
   }
   drawBall();
-  drawPaddles(paddle1T, paddle1Y, paddle2T, paddle2Y);
 }
 
-void resetGame(int paddle1Y,int paddle2Y){
+void resetGame(int paddle1T,int paddle2T){
     ballX = 160;
     ballY = 120;
     ballSpeedX = (random(0,2)) ? 6 : -6;
     ballSpeedY = (random(0,2)) ? random(-1,-4) : random(1, 4);
-    paddle1Y, paddle2Y = 100;
+    paddle1Y = 100;
+    paddle2Y = 100;
+    TFT.fillRect(0, 0, paddleWidth, screenHeight, ILI9341_BLACK);
+    TFT.fillRect(screenWidth - paddleWidth, 0, paddleWidth, screenHeight, ILI9341_BLACK);
+    
+    TFT.fillRect(0, paddle1Y, paddleWidth, paddleHeight, ILI9341_GREEN);
+    TFT.fillRect(screenWidth - paddleWidth, paddle2Y, paddleWidth, paddleHeight, ILI9341_GREEN);
+    drawBall();
+    delay(1000);
 }
 
-void drawPaddles(int paddle1T, int paddle1Y, int paddle2T, int paddle2Y) {
+void drawPaddles(int paddle1T,int paddle2T) {
 
     TFT.fillRect(0, paddle1T, paddleWidth, paddleHeight, ILI9341_BLACK);
     TFT.fillRect(screenWidth - paddleWidth, paddle2T, paddleWidth, paddleHeight, ILI9341_BLACK);
